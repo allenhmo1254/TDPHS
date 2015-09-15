a250e7b02e19f606c944b6da29c7ab6bf1727017
allenhmo1254 <allenhmo1254@sina.com>
2015-09-15 09:30:58 +0800

    1
    1

---
+++ Assets/Script/Player/PlayerTranscriptAnimation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTranscriptAnimation : MonoBehaviour {
+
+//	private Animator anim;
+	// Use this for initialization
+	void Start () {
+//		anim = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		print ("Mathf.Abs(rigidbody.velocity.x) = " + Mathf.Abs (rigidbody.velocity.x));
+//		print ("Mathf.Abs(rigidbody.velocity.z) = " + Mathf.Abs (rigidbody.velocity.z));
+//		if (Mathf.Abs(rigidbody.velocity.x) > 0 || Mathf.Abs(rigidbody.velocity.z) > 0) {
+//			anim.SetBool("Move", true);
+//		} else {
+//			anim.SetBool("Move", false);
+//		}
+	}
+}
---
+++ Assets/Script/Player/PlayerTranscriptMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTranscriptMove : MonoBehaviour {
+
+	private float velocity = 8;
+	// Use this for initialization
+	private Animator anim;
+
+	void Start () {
+		anim = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+//	void Update () {
+//		float h = Input.GetAxis("Horizontal");
+//		float v = Input.GetAxis("Vertical");
+//		Vector3 vel = rigidbody.velocity;
+//		rigidbody.velocity = new Vector3 (h * velocity, vel.y, v * velocity);
+//	    
+//		if (Mathf.Abs (h) > 0.05f || Mathf.Abs (v) > 0.05f) {
+//			transform.rotation = Quaternion.LookRotation(new Vector3(h, 0, v));
+//		}
+//	}
+
+
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		Vector3 vel = rigidbody.velocity;
+
+		
+		if (Mathf.Abs (h) > 0.05f || Mathf.Abs (v) > 0.05f) {
+			anim.SetBool ("Move", true);
+			rigidbody.velocity = new Vector3 (h * velocity, vel.y, v * velocity);
+			transform.LookAt(new Vector3 (h, 0, v) + transform.position);
+		} else {
+			anim.SetBool ("Move", false);
+			rigidbody.velocity = new Vector3 (0, vel.y, 0);
+		}
+	}
+}
